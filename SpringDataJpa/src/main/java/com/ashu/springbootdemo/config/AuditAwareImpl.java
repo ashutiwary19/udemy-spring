327036ce1aee95bdc92e745eeb1cfdb2806b0cda
ashutiwary19 <ashu.tiwary19@gmail.com>
2025-02-28 20:50:23 +0530

    Added spring jpa auditing

---
+++ SpringDataJpa/src/main/java/com/ashu/springbootdemo/config/AuditAwareImpl.java
@@ -0,0 +1,18 @@
+package com.ashu.springbootdemo.config;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+	@Override
+	public @NonNull Optional<String> getCurrentAuditor() {
+		return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getName());
+	}
+
+}
